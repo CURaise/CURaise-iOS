309bc9bc2d20782a1724977a93ab47f01f4833a7
Steven Yu <stevenyuser@gmail.com>
2023-11-19 01:36:45 -0500

    Initial Commit

---
+++ CURaise/CURaiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CURaiseApp.swift
+//  CURaise
+//
+//  Created by Steven Yu on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CURaiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
